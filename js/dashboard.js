5e34261436c2fdd0855a1a976802980f52e9fa3f
admin <kingpatel8122@gmail.com>
2021-05-21 13:02:14 +0530

    Add task and Delete Task completed

--- js/dashboard.js
+++ js/dashboard.js
@@ -33,3 +33,51 @@ const coordinates= ()=>{
     }
 
 }
+
+
+const addTask =()=>{
+     var li = document.createElement("li");
+     var inputTask = document.getElementById("myInputTask").value;
+     var myNewTask = document.createTextNode(inputTask);
+
+    li.appendChild(myNewTask);
+    if (inputTask === ''){
+        alert("Please Enter Anything !!!")
+    } else{
+        document.getElementById('myULTask').appendChild(li);
+    }
+    document.getElementById("myInputTask").value = "";
+    // if (inputValue === '') {
+    //     alert("You must write something!");
+    // } else {
+    //     document.getElementById("myULTask").appendChild(li);
+    // }
+    // document.getElementById("myInputTask").value = "";
+    li.className = "task-list";
+
+    // Create a "close" button and append it to each list item
+    var myNodelist = document.getElementsByClassName("task-list");
+    var i;
+    for (i = 0; i < myNodelist.length; i++) {
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        myNodelist[i].appendChild(span);
+    }
+
+    // Click on a close button to hide the current list item
+    var close = document.getElementsByClassName("close");
+    var i;
+    for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+    }
+}
+
+
+
+
+
