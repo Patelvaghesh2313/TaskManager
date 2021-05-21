33f4e35be8489ac888e89ef971e15d3ed5f45c13
admin <kingpatel8122@gmail.com>
2021-05-21 19:43:17 +0530

    profile work done

--- js/dashboard.js
+++ js/dashboard.js
@@ -46,7 +46,6 @@ const coordinates= ()=>{
 }
 
 
-
 const addTask =()=>{
      var li = document.createElement("li");
      var inputTask = document.getElementById("myInputTask").value;
@@ -113,3 +112,66 @@ const logout = (e)=>{
 }
 
  
+const disableProfile = () =>{
+    var keys = Object.keys(localStorage);
+
+    document.getElementById("tfname").disabled = true;
+    document.getElementById("tlname").disabled = true;
+    document.getElementById("temail").disabled = true;
+    document.getElementById("tmobile").disabled = true;
+    document.getElementById("tbtnsave").disabled = true;
+
+    var loggedUser = sessionStorage.getItem("Email");
+
+    for( key of keys){
+        var data = JSON.parse(localStorage.getItem(key));
+        if(data.email===loggedUser){
+            var usedEmail = data.email;
+            var userFname = data.fname
+            document.getElementById("tfname").value = data.fname;
+            document.getElementById("tlname").value = data.lname;
+            document.getElementById("temail").value = data.email;
+            document.getElementById("tmobile").value = data.mobile;
+            
+            alert(`User Already Registered With This Email...${usedEmail} and ${userFname}`);
+        }
+    }
+}
+
+const enableProfile = () =>{
+    document.getElementById("tfname").disabled = false;
+    document.getElementById("tlname").disabled = false;
+    document.getElementById("temail").disabled = false;
+    document.getElementById("tmobile").disabled = false;
+    document.getElementById("tbtnsave").disabled = false;
+}
+
+const updateProfile = () =>{
+
+    var keys = Object.keys(localStorage);
+
+    let pfname = document.getElementById('tfname');
+    let plname =  document.getElementById('tlname');
+    let pemail = document.getElementById('temail');
+    let pmobile = document.getElementById('tmobile');
+
+    var authUser = sessionStorage.getItem("Email");
+
+    for( key of keys){
+        var data = JSON.parse(localStorage.getItem(key));
+        if(data.email===authUser){
+            var usedEmail = data.email;
+            let user = {
+                fname: pfname.value,
+                lname: plname.value,
+                email: pemail.value,
+                mobile: pmobile.value,
+                dob: data.dob,
+                gender: data.gender,
+                password: data.password
+                }
+            localStorage.setItem(key, JSON.stringify(user));
+        }
+    }
+
+}
